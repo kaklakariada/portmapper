4a075df2da75c7d525b740bf68579e99a5f8ec04
christoph <christoph@2bd42da9-18a5-434a-ad8e-d1ed5d6a128d>
2010-12-18 18:40:15 +0000

    added handler for mac about and settings menu
    git-svn-id: http://svn.code.sf.net/p/upnp-portmapper/code/trunk@87 2bd42da9-18a5-434a-ad8e-d1ed5d6a128d

--- src/org/chris/portmapper/PortMapperStarter.java
+++ src/org/chris/portmapper/PortMapperStarter.java
@@ -3,6 +3,12 @@
  */
 package org.chris.portmapper;
 
+import com.apple.eawt.AboutHandler;
+import com.apple.eawt.AppEvent.AboutEvent;
+import com.apple.eawt.AppEvent.PreferencesEvent;
+import com.apple.eawt.Application;
+import com.apple.eawt.PreferencesHandler;
+
 /**
  * @author chris
  * @version $Id$
@@ -13,8 +19,41 @@ public class PortMapperStarter {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		if (System.getProperty("os.name").startsWith("Mac")) {
+			setupMacOptions();
+		}
 		PortMapperCli cli = new PortMapperCli();
 		cli.start(args);
+	}
+
+	private static void setupMacOptions() {
+		System.setProperty("com.apple.mrj.application.apple.menu.about.name",
+				"UPnP PortMapper");
+		System.setProperty("com.apple.macos.useScreenMenuBar", "true");
+		System.setProperty("apple.awt.brushMetalLook", "false");
+		System.setProperty("com.apple.mrj.application.growbox.intrudes",
+				"false");
+		System.setProperty("com.apple.mrj.application.live-resize", "true");
+
+		Application app = Application.getApplication();
+		app.setPreferencesHandler(new PreferencesHandler() {
+			public void handlePreferences(PreferencesEvent arg0) {
+				PortMapperApp.getInstance().getView().changeSettings();
+			}
+		});
+		app.setAboutHandler(new AboutHandler() {
+			public void handleAbout(AboutEvent arg0) {
+				PortMapperApp.getInstance().getView().showAboutDialog();
+			}
+		});
+		//
+		// File iconFile = new File("internet_device_256.png");
+		// try {
+		// app.setDockIconImage(ImageIO.read(iconFile));
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
 	}
 
 }
