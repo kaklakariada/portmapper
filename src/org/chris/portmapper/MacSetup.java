bc4487971390bde3af57bc0e3d23d956e64937ff
christoph <christoph@2bd42da9-18a5-434a-ad8e-d1ed5d6a128d>
2013-07-14 15:45:02 +0000

    fixed warning
    git-svn-id: http://svn.code.sf.net/p/upnp-portmapper/code/branches/gradle@115 2bd42da9-18a5-434a-ad8e-d1ed5d6a128d

--- src/org/chris/portmapper/MacSetup.java
+++ src/org/chris/portmapper/MacSetup.java
@@ -18,20 +18,20 @@ public class MacSetup {
 	public static void setupMac() {
 		System.setProperty("com.apple.mrj.application.apple.menu.about.name",
 				"UPnP PortMapper");
-		System.setProperty("com.apple.macos.useScreenMenuBar", "true");
+		System.setProperty("apple.laf.useScreenMenuBar", "true");
 		System.setProperty("apple.awt.brushMetalLook", "false");
 		System.setProperty("com.apple.mrj.application.growbox.intrudes",
 				"false");
 		System.setProperty("com.apple.mrj.application.live-resize", "true");
 
-		Application app = Application.getApplication();
+		final Application app = Application.getApplication();
 		app.setPreferencesHandler(new PreferencesHandler() {
-			public void handlePreferences(PreferencesEvent arg0) {
+			public void handlePreferences(final PreferencesEvent arg0) {
 				PortMapperApp.getInstance().getView().changeSettings();
 			}
 		});
 		app.setAboutHandler(new AboutHandler() {
-			public void handleAbout(AboutEvent arg0) {
+			public void handleAbout(final AboutEvent arg0) {
 				PortMapperApp.getInstance().getView().showAboutDialog();
 			}
 		});
