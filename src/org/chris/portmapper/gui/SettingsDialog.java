6eb9ddce4314589f6dbb2dd4bd2aa98e359c100b
kaklakariada <chris@pirkls-imac.fritz.box>
2014-02-11 15:41:44 +0100

    add cling to list of available libs

--- src/org/chris/portmapper/gui/SettingsDialog.java
+++ src/org/chris/portmapper/gui/SettingsDialog.java
@@ -20,6 +20,7 @@ import net.miginfocom.swing.MigLayout;
 
 import org.chris.portmapper.PortMapperApp;
 import org.chris.portmapper.Settings;
+import org.chris.portmapper.router.cling.ClingRouterFactory;
 import org.chris.portmapper.router.dummy.DummyRouterFactory;
 import org.chris.portmapper.router.sbbi.SBBIRouterFactory;
 import org.chris.portmapper.router.weupnp.WeUPnPRouterFactory;
@@ -37,123 +38,112 @@ import ch.qos.logback.classic.Level;
  */
 public class SettingsDialog extends JDialog {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	private final static String DIALOG_NAME = "settings_dialog";
+    private final static String DIALOG_NAME = "settings_dialog";
 
-	private final static String ACTION_SAVE = DIALOG_NAME + ".save";
-	private final static String ACTION_CANCEL = DIALOG_NAME + ".cancel";
+    private final static String ACTION_SAVE = DIALOG_NAME + ".save";
+    private final static String ACTION_CANCEL = DIALOG_NAME + ".cancel";
 
-	private JCheckBox useEntityEncoding;
+    private JCheckBox useEntityEncoding;
 
-	private JComboBox<Level> logLevelComboBox;
-	private JComboBox<String> routerFactoryClassComboBox;
+    private JComboBox<Level> logLevelComboBox;
+    private JComboBox<String> routerFactoryClassComboBox;
 
-	private JButton okButton;
+    private JButton okButton;
 
-	private final PortMapperApp app;
+    private final PortMapperApp app;
 
-	public SettingsDialog(final PortMapperApp app) {
-		super(app.getMainFrame(), true);
-		this.app = app;
+    public SettingsDialog(final PortMapperApp app) {
+        super(app.getMainFrame(), true);
+        this.app = app;
 
-		logger.debug("Create settings dialog");
-		this.setContentPane(this.getDialogPane());
+        logger.debug("Create settings dialog");
+        this.setContentPane(this.getDialogPane());
 
-		this.getRootPane().setDefaultButton(okButton);
-		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		this.setName(DIALOG_NAME);
-		this.setModal(true);
-		this.pack();
+        this.getRootPane().setDefaultButton(okButton);
+        this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        this.setName(DIALOG_NAME);
+        this.setModal(true);
+        this.pack();
 
-		// Register an action listener that closes the window when the ESC
-		// button is pressed
-		final KeyStroke escKeyStroke = KeyStroke.getKeyStroke(
-				KeyEvent.VK_ESCAPE, 0, true);
-		final ActionListener windowCloseActionListener = new ActionListener() {
-			@Override
-			public final void actionPerformed(final ActionEvent e) {
-				cancel();
-			}
-		};
-		getRootPane().registerKeyboardAction(windowCloseActionListener,
-				escKeyStroke, JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-	}
+        // Register an action listener that closes the window when the ESC
+        // button is pressed
+        final KeyStroke escKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, true);
+        final ActionListener windowCloseActionListener = new ActionListener() {
+            @Override
+            public final void actionPerformed(final ActionEvent e) {
+                cancel();
+            }
+        };
+        getRootPane().registerKeyboardAction(windowCloseActionListener, escKeyStroke,
+                JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
 
-	private static JLabel createLabel(final String name) {
-		final JLabel newLabel = new JLabel(name);
-		newLabel.setName(name);
-		return newLabel;
-	}
+    private static JLabel createLabel(final String name) {
+        final JLabel newLabel = new JLabel(name);
+        newLabel.setName(name);
+        return newLabel;
+    }
 
-	private JPanel getDialogPane() {
-		final ActionMap actionMap = app.getContext().getActionMap(
-				this.getClass(), this);
-		final Settings settings = app.getSettings();
+    private JPanel getDialogPane() {
+        final ActionMap actionMap = app.getContext().getActionMap(this.getClass(), this);
+        final Settings settings = app.getSettings();
 
-		final JPanel dialogPane = new JPanel(new MigLayout("", // Layout
-				// Constraints
-				"[right]rel[left,grow 100]", // Column Constraints
-				"")); // Row Constraints
+        final JPanel dialogPane = new JPanel(new MigLayout("", // Layout
+                // Constraints
+                "[right]rel[left,grow 100]", // Column Constraints
+                "")); // Row Constraints
 
-		logger.debug("Use entity encoding is " + settings.isUseEntityEncoding());
-		useEntityEncoding = new JCheckBox(
-				"settings_dialog.use_entity_encoding",
-				settings.isUseEntityEncoding());
-		useEntityEncoding.setName("settings_dialog.use_entity_encoding");
+        logger.debug("Use entity encoding is " + settings.isUseEntityEncoding());
+        useEntityEncoding = new JCheckBox("settings_dialog.use_entity_encoding", settings.isUseEntityEncoding());
+        useEntityEncoding.setName("settings_dialog.use_entity_encoding");
 
-		dialogPane.add(useEntityEncoding, "span 2, wrap");
+        dialogPane.add(useEntityEncoding, "span 2, wrap");
 
-		dialogPane.add(createLabel("settings_dialog.log_level"));
+        dialogPane.add(createLabel("settings_dialog.log_level"));
 
-		logLevelComboBox = new JComboBox<>(new Vector<>(Arrays.asList(
-				Level.ALL, Level.TRACE, Level.DEBUG, Level.INFO, Level.WARN,
-				Level.ERROR, Level.OFF)));
-		logLevelComboBox.setSelectedItem(Level.toLevel(settings.getLogLevel()));
+        logLevelComboBox = new JComboBox<>(new Vector<>(Arrays.asList(Level.ALL, Level.TRACE, Level.DEBUG, Level.INFO,
+                Level.WARN, Level.ERROR, Level.OFF)));
+        logLevelComboBox.setSelectedItem(Level.toLevel(settings.getLogLevel()));
 
-		dialogPane.add(logLevelComboBox, "wrap");
+        dialogPane.add(logLevelComboBox, "wrap");
 
-		dialogPane.add(createLabel("settings_dialog.upnp_lib"));
+        dialogPane.add(createLabel("settings_dialog.upnp_lib"));
 
-		routerFactoryClassComboBox = new JComboBox<>(new Vector<>(
-				Arrays.asList(SBBIRouterFactory.class.getName(),
-						WeUPnPRouterFactory.class.getName(),
-						DummyRouterFactory.class.getName())));
-		routerFactoryClassComboBox.setSelectedItem(settings
-				.getRouterFactoryClassName());
-		dialogPane.add(routerFactoryClassComboBox, "span 2, wrap");
+        routerFactoryClassComboBox = new JComboBox<>(new Vector<>(Arrays.asList(SBBIRouterFactory.class.getName(),
+                WeUPnPRouterFactory.class.getName(), ClingRouterFactory.class.getName(),
+                DummyRouterFactory.class.getName())));
+        routerFactoryClassComboBox.setSelectedItem(settings.getRouterFactoryClassName());
+        dialogPane.add(routerFactoryClassComboBox, "span 2, wrap");
 
-		dialogPane.add(new JButton(actionMap.get(ACTION_CANCEL)),
-				"tag cancel, span 2");
-		okButton = new JButton(actionMap.get(ACTION_SAVE));
-		dialogPane.add(okButton, "tag ok, wrap");
+        dialogPane.add(new JButton(actionMap.get(ACTION_CANCEL)), "tag cancel, span 2");
+        okButton = new JButton(actionMap.get(ACTION_SAVE));
+        dialogPane.add(okButton, "tag ok, wrap");
 
-		return dialogPane;
-	}
+        return dialogPane;
+    }
 
-	/**
-	 * This method is executed when the user clicks the save button. The method
-	 * saves the entered preset
-	 */
-	@Action(name = ACTION_SAVE)
-	public void save() {
-		final Settings settings = app.getSettings();
-		settings.setUseEntityEncoding(useEntityEncoding.isSelected());
-		settings.setLogLevel(((Level) logLevelComboBox.getSelectedItem())
-				.toString());
-		settings.setRouterFactoryClassName(routerFactoryClassComboBox
-				.getSelectedItem().toString());
+    /**
+     * This method is executed when the user clicks the save button. The method saves the entered preset
+     */
+    @Action(name = ACTION_SAVE)
+    public void save() {
+        final Settings settings = app.getSettings();
+        settings.setUseEntityEncoding(useEntityEncoding.isSelected());
+        settings.setLogLevel(((Level) logLevelComboBox.getSelectedItem()).toString());
+        settings.setRouterFactoryClassName(routerFactoryClassComboBox.getSelectedItem().toString());
 
-		app.setLogLevel(settings.getLogLevel());
+        app.setLogLevel(settings.getLogLevel());
 
-		logger.debug("Saved settings " + settings);
-		this.dispose();
-	}
+        logger.debug("Saved settings " + settings);
+        this.dispose();
+    }
 
-	@Action(name = ACTION_CANCEL)
-	public void cancel() {
-		this.dispose();
-	}
+    @Action(name = ACTION_CANCEL)
+    public void cancel() {
+        this.dispose();
+    }
 }
