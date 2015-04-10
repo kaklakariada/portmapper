d9ae16952cebd1a587dadffbae29d4ca35fc88b8
kaklakariada <kaklakariada@users.noreply.github.com>
2015-04-10 22:12:54 +0200

    redirect java.util.logging to logback

--- src/main/java/org/chris/portmapper/PortMapperStarter.java
+++ src/main/java/org/chris/portmapper/PortMapperStarter.java
@@ -2,12 +2,14 @@ package org.chris.portmapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 public class PortMapperStarter {
 
     private final static Logger LOG = LoggerFactory.getLogger(PortMapperStarter.class);
 
     public static void main(final String[] args) {
+        redirectJavaUtilLoggingToLogback();
         final PortMapperCli cli = new PortMapperCli();
         try {
             cli.start(args);
@@ -15,5 +17,10 @@ public class PortMapperStarter {
             LOG.error("PortMapper failed with exception " + e.getMessage(), e);
             System.exit(1);
         }
+    }
+
+    private static void redirectJavaUtilLoggingToLogback() {
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
     }
 }
